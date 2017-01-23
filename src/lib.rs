61bd30ec6a0dd836f35b37af67e588be1d8ffe0c
David Tolnay <dtolnay@gmail.com>
2017-01-23 00:23:33 -0800

    Support nightly-2017-01-21

--- src/lib.rs
+++ src/lib.rs
@@ -36,9 +36,10 @@ macro_rules! proc_macro_expr_impl {
             #[proc_macro_derive($func)]
             pub fn $func(input: $crate::TokenStream) -> $crate::TokenStream {
                 let source = input.to_string();
+                let source = source.trim();
 
                 let prefix = "#[allow(unused)]\nenum ProcMacroHack { Input = (stringify!(";
-                let suffix = "), 0).1, }\n";
+                let suffix = "), 0).1, }";
                 if !(source.starts_with(prefix) && source.ends_with(suffix)) {
                     panic!("`{}` procedural macro failed", stringify!($func));
                 }
@@ -70,9 +71,10 @@ macro_rules! proc_macro_item_impl {
             #[proc_macro_derive($func)]
             pub fn $func(input: $crate::TokenStream) -> $crate::TokenStream {
                 let source = input.to_string();
+                let source = source.trim();
 
                 let prefix = "#[allow(unused)]\nenum ProcMacroHack { Input = (stringify!(";
-                let suffix = "), 0).1, }\n";
+                let suffix = "), 0).1, }";
                 if !(source.starts_with(prefix) && source.ends_with(suffix)) {
                     panic!("`{}` procedural macro failed", stringify!($func));
                 }
