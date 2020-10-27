a991a7eca28f2730384173b9b18bfd87496a7f64
David Tolnay <dtolnay@gmail.com>
2020-10-27 16:38:36 -0700

    Add CI coverage of only_hack_old_rustc

--- demo-hack/src/lib.rs
+++ demo-hack/src/lib.rs
@@ -3,5 +3,5 @@ use proc_macro_hack::proc_macro_hack;
 /// Add one to an expression.
 ///
 /// (Documentation goes here on the re-export, not in the other crate.)
-#[proc_macro_hack]
+#[proc_macro_hack(only_hack_old_rustc)]
 pub use demo_hack_impl::add_one;
